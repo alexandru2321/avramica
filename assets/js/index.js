3bba89791c9ca88b45467d8bdbc0eeb8ede4a509
EINazare <emanuelioannazare@gmail.com>
2021-01-13 20:49:11 +0200

    Updates

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,6 +2,6 @@
   let startYear = document.getElementById("footer-start-year").innerText;
   let endYear = new Date().getFullYear();
   if (startYear.toString() !== endYear.toString()) {
-    document.getElementById("footer-end-year").innerText = "-" + startYear;
+    document.getElementById("footer-end-year").innerText = "-" + endYear;
   }
 })();
