2f73260687108f558d94ecd1439fe2cdf7284dcc
alexandru2321 <alexandru2321@yahoo.com>
2021-10-29 15:29:50 +0300

    schimb mai multe :))

--- assets/js/index.js
+++
@@ -1,7 +0,0 @@
-(function () {
-  let startYear = document.getElementById("footer-start-year").innerText;
-  let endYear = new Date().getFullYear();
-  if (startYear.toString() !== endYear.toString()) {
-    document.getElementById("footer-end-year").innerText = "-" + endYear;
-  }
-})();
